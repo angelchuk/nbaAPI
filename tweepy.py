511e6e2e56438fc534c6be7a5234d1f3f139c326
Angel Chukwuma <angel.oluchuk@gmail.com>
2024-06-19 18:57:30 +0000

    got tweepy tokens

---
+++ tweepy.py
@@ -0,0 +1,7 @@
+import tweepy
+
+#consumer_key = 'YOUR_CONSUMER_KEY'
+#consumer_secret = 'YOUR_CONSUMER_SECRET'
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAAuTuQEAAAAA18rowkTyIpehI1Wa50RGVVWNRqI%3DCfb0XaihOX8I3fSBhTBrYJWdpu6iWeuORvlkakuuHafdfjPOQ4'
+
+print(10//3)
